21f6ce0cacf339cc9105fd4925e38b94b203f432
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-07-08 09:33:19 -0400

    chore:

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = ({ payment }: any) => {
 
+  console.log(payment);
+
   return (
     <div className={styles.container}>
       <Head>
