3dc4d27ad7b19096244eb6d46ea049de9de29980
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-07-09 18:58:38 -0400

    chore:

--- src/components/Patrician/index.tsx
+++ src/components/Patrician/index.tsx
@@ -45,13 +45,13 @@ const PatracianList = [
 
 const Patrician: React.FC = () => {
   return (
-    <div className="flex items-center justify-center bg-neutral-900 pt-5 pb-5">
+    <div className="flex items-center justify-center bg-white pt-5 pb-5">
       <div className="p-4 md:w-[1200px]">
-      <h1>Da uma espiadinha nas empresas que estarão presentes...</h1>
+        <h1 className="text-black text-center">Da uma espiadinha nas empresas que estarão presentes...</h1>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-3 mt-4">
         {PatracianList.map((patracian) => {
           return (
-            <div className="text-center font-bold">
+            <div className="text-center font-bold border-2 border-black/5 p-3 rounded-md">
               <img src={patracian.image} className="rounded-sm" />
               <h1>{patracian.title}</h1>
             </div>
