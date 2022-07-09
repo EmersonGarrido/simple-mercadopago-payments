d021c3009254d991f56910aa6bb9e7b2e53e60ae
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-07-09 19:49:58 -0400

    chore: push

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const CardProduct: React.FC = () => {
   return (
-    <div className="md:h-[200px] hover:bg-zinc-100/50 hover:shadow-black-200 h-[150px] flex bg-white rounded-lg border-2 border-[#F1F1F1]" style={{
+    <div className="md:h-[200px] h-[170px] hover:bg-zinc-100/50 hover:shadow-black-200 flex bg-white rounded-lg border-2 border-[#F1F1F1]" style={{
       boxShadow: "0px 3px 1px -2px rgba(0, 0, 0, 0.06), 0px 5px 3px -2px rgba(0, 0, 0, 0.02)"
     }}>
-      <img src="/event.jpeg" className="md:w-[200px] md:h-[200px] h-[150px] rounded-tl-lg rounded-bl-lg" alt="" />
+      <img src="/event.jpeg" className="md:w-[200px] md:h-[200px] h-[170px] rounded-tl-lg rounded-bl-lg" alt="" />
       <div className="p-4 w-full flex flex-col items-center justify-between gap-3">
         <div className="w-full">
           <div className="flex md:items-start md:justify-between md:flex-row flex-col">
-            <div className="text-black font-bold">Experiência Completa</div>
-            <div className="text-black font-bold">R$100,00</div>
+            <div className="text-[#7D7D7D] font-bold">Experiência Completa</div>
+            <div className="text-[#7D7D7D] font-bold">R$100,00</div>
           </div>
           <p className="text-[#B4B4B4] md:flex hidden">
             Design, inovação, qualidade...
@@ -19,9 +19,9 @@ const CardProduct: React.FC = () => {
             Saiba mais
           </p>
         </div>
-        <div className="w-full flex md:justify-between md:items-center md:flex-row flex-col">
-          <button className="text-black/50 font-bold bg-[#E4E4E4] p-2 pl-3 pr-3 rounded-full">Comprar Agora</button>
-          <div className="text-[#B4B4B4] text-center md:text-end">Compartilhar</div>
+        <div className="w-full flex md:justify-between md:items-center md:flex-row flex-col gap-2">
+          <button className="text-black/50 font-bold hover:bg-[#d0cfcf] bg-[#E4E4E4] p-2 pl-3 pr-3 rounded-full">Comprar Agora</button>
+          <div className="text-[#B4B4B4] text-center md:text-end cursor-pointer hover:text-black">Compartilhar</div>
         </div>
       </div>
     </div>
